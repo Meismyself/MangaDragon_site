884ce20d7363bb45cccc342702332dea979bf6c2
mycourses1@gmail.com <mycourses1@gmail.com>
2023-02-22 05:32:58 +0100

    heelllo

---
+++ main.js
@@ -0,0 +1,79 @@
+// Swiper manga must reading
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 6.3,
+  spaceBetween: 30,
+  slideClass: 'slide',
+  centeredSlides: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+// End Function
+
+// Sign function dropdown
+const DropBtn = document.getElementsByClassName('.DropBtn');
+const DropDown = document.getElementById('dropdown-content');
+let close = true;
+
+function OpenDropDown() {
+  DropDown.style.display = 'none'
+  if (close === true) {
+    DropDown.style.display = 'inherit'
+    close = false;
+  } else if (close === false) {
+    DropDown.style.display = 'none'
+    close = true;
+  }
+}
+// mustShow..................................
+  const mustDay = document.getElementById('mustWatch-Grid-Day');
+  const mustWeek = document.getElementById('mustWatch-Grid-Week');
+  const mustMonth = document.getElementById('mustWatch-Grid-Month');
+function OpenmustDay() {
+    mustDay.style.transform = 'translateX(0%)'
+    mustWeek.style.transform = 'translateX(0%)'
+    mustMonth.style.transform = 'translateX(0%)'
+}
+function OpenmustWeek() {
+  
+      mustDay.style.transform = 'translateX(100%)'
+      mustWeek.style.transform = 'translateX(100%)'
+      mustMonth.style.transform = 'translateX(100%)'
+}
+function OpenmustMonth() {
+      mustDay.style.transform = 'translateX(200%)'
+      mustWeek.style.transform = 'translateX(200%)'
+      mustMonth.style.transform = 'translateX(200%)'
+}
+// End .......,..............................
+// Bestseries
+const bestDay = document.getElementById('BestSeries-Grid-Day');
+const bestWeek = document.getElementById('BestSeries-Grid-Week');
+const bestMonth = document.getElementById('BestSeries-Grid-Month');
+function OpenBestDay() {
+    bestDay.style.transform = 'translateX(0%)'
+    bestWeek.style.transform = 'translateX(0%)'
+    bestMonth.style.transform = 'translateX(0%)'
+}
+function OpenBestWeek() {
+  
+      bestDay.style.transform = 'translateX(100%)'
+      bestWeek.style.transform = 'translateX(100%)'
+      bestMonth.style.transform = 'translateX(100%)'
+}
+function OpenBestMonth() {
+      bestDay.style.transform = 'translateX(200%)'
+      bestWeek.style.transform = 'translateX(200%)'
+      bestMonth.style.transform = 'translateX(200%)'
+}
+// End
+document.getElementById('search').addEventListener('keyup', function(event) {
+  let searchQuery = event.target.value;
+  console.log(searchQuery)
+})
