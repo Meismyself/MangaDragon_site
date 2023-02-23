5192fd8e6177fba6abcfba72676dcaaf0111f5d5
mycourses1@gmail.com <mycourses1@gmail.com>
2023-02-23 02:10:04 +0100

    add som changes

--- main.js
+++ main.js
@@ -32,52 +32,24 @@ function OpenDropDown() {
   }
 }
 // mustShow..................................
-const mustDay = document.getElementById('mustWatch-Grid-Day');
-const mustWeek = document.getElementById('mustWatch-Grid-Week');
-const mustMonth = document.getElementById('mustWatch-Grid-Month');
-
-function OpenmustDay() {
-  mustDay.style.transform = 'translateX(0%)'
-  mustWeek.style.transform = 'translateX(0%)'
-  mustMonth.style.transform = 'translateX(0%)'
-}
-
-function OpenmustWeek() {
-
-  mustDay.style.transform = 'translateX(100%)'
-  mustWeek.style.transform = 'translateX(100%)'
-  mustMonth.style.transform = 'translateX(100%)'
-}
-
-function OpenmustMonth() {
-  mustDay.style.transform = 'translateX(200%)'
-  mustWeek.style.transform = 'translateX(200%)'
-  mustMonth.style.transform = 'translateX(200%)'
-}
+document.getElementById('mustWatch-Day').addEventListener('click', function(){
+  document.getElementById('mustWatch-Grid-Day').style.transform = 'translateX(0%)'
+document.getElementById('mustWatch-Grid-Week').style.transform = 'translateX(0%)'
+document.getElementById('mustWatch-Grid-Month').style.transform = 'translateX(0%)'
+})
+document.getElementById('mustWatch-Week').addEventListener('click', function(){
+  document.getElementById('mustWatch-Grid-Day').style.transform = 'translateX(100%)'
+  document.getElementById('mustWatch-Grid-Week').style.transform = 'translateX(100%)'
+  document.getElementById('mustWatch-Grid-Month').style.transform = 'translateX(100%)'
+})
+document.getElementById('mustWatch-Month').addEventListener('click', function(){
+document.getElementById('mustWatch-Grid-Day').style.transform = 'translateX(200%)'
+document.getElementById('mustWatch-Grid-Week').style.transform = 'translateX(200%)'
+document.getElementById('mustWatch-Grid-Month').style.transform = 'translateX(200%)'
+})
 // End .......,..............................
 // Bestseries
-const bestDay = document.getElementById('BestSeries-Grid-Day');
-const bestWeek = document.getElementById('BestSeries-Grid-Week');
-const bestMonth = document.getElementById('BestSeries-Grid-Month');
 
-function OpenBestDay() {
-  bestDay.style.transform = 'translateX(0%)'
-  bestWeek.style.transform = 'translateX(0%)'
-  bestMonth.style.transform = 'translateX(0%)'
-}
-
-function OpenBestWeek() {
-
-  bestDay.style.transform = 'translateX(100%)'
-  bestWeek.style.transform = 'translateX(100%)'
-  bestMonth.style.transform = 'translateX(100%)'
-}
-
-function OpenBestMonth() {
-  bestDay.style.transform = 'translateX(200%)'
-  bestWeek.style.transform = 'translateX(200%)'
-  bestMonth.style.transform = 'translateX(200%)'
-}
 // End
 document.getElementById('search').addEventListener('keyup', function(event) {
   let searchQuery = event.target.value;
